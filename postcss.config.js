fd36873ca73c2641d11741a63817b580f1b8b5be
KarasuTatehi <45784893+KarasuTatehi@users.noreply.github.com>
2021-02-26 08:28:42 +0900

    不要なファイルを削除

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require("autoprefixer")({ grid: true }),
-    require("cssnano"),
-    require("postcss-sorting")({
-      "properties-order": ["margin", "padding", "border", "background"]
-    })
-  ]
-};
