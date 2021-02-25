42422c48eeeac89e65125857ed9413b82aa41e55
KarasuTatehi <45784893+KarasuTatehi@users.noreply.github.com>
2021-02-26 08:45:20 +0900

    imageminを追加

---
+++ imagemin.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "mozjpeg": {
+    "progressive": true,
+    "quality": 65,
+  },
+  "pngquant": {
+    "quality": 65,
+    "speed": 4,
+  },
+  "gifsicle": {
+    "interlaced": false,
+    "optimizationLevel": 3,
+  },
+  "webp": { quality: 25 },
+};
